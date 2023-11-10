5c609f4720bb43c923df8d7ab7d3d381bc6cc9de
Jessica Salvino <150205230+heysalvino@users.noreply.github.com>
2023-11-10 15:15:41 -0500

    Update MainActivity.java

--- RadioRollins-master/app/src/main/java/cms/java/htmltoweb/MainActivity.java
+++ RadioRollins-master/app/src/main/java/cms/java/htmltoweb/MainActivity.java
@@ -2,7 +2,6 @@ package cms.java.htmltoweb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -11,97 +10,96 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.ImageButton;
-
 import java.io.IOException;
-
 import static android.webkit.WebView.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton b1;
-    MediaPlayer mediaPlayer = new MediaPlayer();
-
     private WebView webView;
-
-
-
-
+    private ImageButton streamButton;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar setup
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        webView = (WebView) findViewById(R.id.webview);
+        // WebView setup
+        webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://wprk.org/about-us/contact-us/");
-
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        // MediaPlayer setup
+        setupMediaPlayer();
 
-        //TODO: add text/image to button and change when ready/play/paused
-        //TODO: If using image, must convert Button to MediaButton
+        // Stream button setup
+        setupStreamButton();
+    }
 
-        //set streaming url
+    private void setupMediaPlayer() {
+        // MediaPlayer setup
         Uri wprkStream = Uri.parse("http://s9.voscast.com:7024");
         try {
-
-            mediaPlayer.setDataSource(getApplicationContext(),wprkStream);
+            mediaPlayer.setDataSource(getApplicationContext(), wprkStream);
         } catch (IOException e) {
             e.printStackTrace();
+            // Handle the error (e.g., show a toast, log the error)
         }
 
-        //prepare the stream, and dont enable the button
+        // Prepare the stream, and don't enable the button
         mediaPlayer.prepareAsync();
-        b1 = (ImageButton) findViewById(R.id.streamButton);
-        b1.setEnabled(false);
-        b1.setImageResource(R.drawable.baseline_loop_24);
-
-        //when mediaplayer is prepared and ready to stream, enable the button
         mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                b1.setImageResource(R.drawable.baseline_play_circle_filled_24);
-                b1.setEnabled(true);
+                streamButton.setImageResource(R.drawable.baseline_play_circle_filled_24);
+                streamButton.setEnabled(true);
             }
         });
+    }
 
-        //when the button is clicked, pause or play the music
-        b1.setOnClickListener(new View.OnClickListener() {
+    private void setupStreamButton() {
+        // Stream button setup
+        streamButton = findViewById(R.id.streamButton);
+        streamButton.setEnabled(false);
+        streamButton.setImageResource(R.drawable.baseline_loop_24);
+
+        // When the button is clicked, pause or play the music
+        streamButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (mediaPlayer.isPlaying()){
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
-                    b1.setImageResource(R.drawable.baseline_play_circle_filled_24);
-
+                    streamButton.setImageResource(R.drawable.baseline_play_circle_filled_24);
                 } else {
                     mediaPlayer.start();
-                    b1.setImageResource(R.drawable.baseline_pause_circle_filled_24);
-
+                    streamButton.setImageResource(R.drawable.baseline_pause_circle_filled_24);
                 }
             }
         });
-
     }
-
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack())
-        {
+        if (webView.canGoBack()) {
             webView.goBack();
-        }
-        else
-        {
+        } else {
             super.onBackPressed();
         }
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
